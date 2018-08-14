e0e75951ed52b39b3e7d44f2f6777b8c27c3c8e2
Ricardo Pereira <rpdesignerfly@gmail.com>
2018-08-14 16:51:48 -0300

    Configurada a estrutura básica compilável dos assets

--- resources/js/boot.js
+++ resources/js/boot.js
@@ -4,7 +4,6 @@ window._ = require('lodash');
 try {
 
     require('html5shiv');
-    require('bootstrap');
 
     window.$ = window.jQuery = require('jquery');
     window.Popper = require('popper.js');
@@ -20,17 +19,3 @@ try {
 
 window.axios = require('axios');
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
-/**
- * Por conveniência, é registrado o token CSRF como um cabeçalho no Axios, de
- * modo que todas as solicitações HTTP sejam efetuadas automaticamente com o token.
- */
-
-let token = document.head.querySelector('meta[name="csrf-token"]');
-
-if (token) {
-    window.csrf_token = token.content;
-    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
-} else {
-    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
-}
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,5 @@
 let mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'assets/js');
-mix.sass('resources/sass/themes/plexi.scss', 'assets/css');
+mix.sass('resources/sass/app.scss', 'assets/css');
 mix.copy('node_modules/@fortawesome/fontawesome-free/webfonts', 'assets/fonts/fontawesome5');
